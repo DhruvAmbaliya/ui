ebece0937eb10266596cac741f2486c7a6f3cb33
DhruvAmbaliya <dhruvambaliya72@gmail.com>
2024-03-08 13:12:59 +0530

    add

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 import '@/styles/globals.css'
-
+import { useEffect, useState } from 'react'
 import Layout from '../components/Layout';
  
 export default function MyApp({ Component, pageProps }) {
+  const [showChild, setShowChild] = useState(false)
+
+  useEffect(() => {
+    setShowChild(true)
+  }, [])
+
+  if (!showChild) {
+    return null
+  }
+
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  );
+  )
 }
